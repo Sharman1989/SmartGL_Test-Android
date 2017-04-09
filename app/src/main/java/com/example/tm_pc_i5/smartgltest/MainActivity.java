cb30649c32ac8e2cb443ea3638e9577bf53e0123
Saeed1989 <saeed.sharman@gamil.com>
2017-04-09 17:29:23 +0600

    initial commit

---
+++ app/src/main/java/com/example/tm_pc_i5/smartgltest/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.tm_pc_i5.smartgltest;
+
+/*
+    Copyright 2016 Arnaud Guyon
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+ */
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import fr.arnaudguyon.smartgl.opengl.OpenGLRenderer;
+import fr.arnaudguyon.smartgl.opengl.SmartGLRenderer;
+import fr.arnaudguyon.smartgl.opengl.SmartGLView;
+
+
+
+public class MainActivity extends Activity {
+
+    private SmartGLView mActivityGLView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onPause() {
+        if (mActivityGLView != null) {
+            mActivityGLView.onPause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mActivityGLView != null) {
+            mActivityGLView.onResume();
+        }
+    }
+
+
+}
+
